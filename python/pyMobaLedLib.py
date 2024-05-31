a2e1cde06c90522574eb31bce1072e0709d66df4
haroldlinke <43720258+haroldlinke@users.noreply.github.com>
2024-05-31 13:40:01 +0200

    V5.3.1
    error in update pyMobaLedlib corrected

--- python/pyMobaLedLib.py
+++ python/pyMobaLedLib.py
@@ -2798,6 +2798,50 @@ class pyMobaLedLibapp(tk.Tk):
                 self._send_ledcolor_to_ARDUINO(lednum, ledcount, "#000000")
                 self.on_ledon = True
                 self.after(int(500/BLINKFRQ),self.onblink_led)
+    
+    def copytree(self, src, dst, symlinks=False, ignore=None):
+        names = os.listdir(src)
+        if ignore is not None:
+            ignored_names = ignore(src, names)
+        else:
+            ignored_names = set()
+            
+        if not os.path.exists(dst):
+            os.makedirs(dst)
+            
+        errors = []
+        for name in names:
+            if name in ignored_names:
+                continue
+            srcname = os.path.join(src, name)
+            dstname = os.path.join(dst, name)
+            try:
+                if symlinks and os.path.islink(srcname):
+                    linkto = os.readlink(srcname)
+                    os.symlink(linkto, dstname)
+                elif os.path.isdir(srcname):
+                    self.copytree(srcname, dstname, symlinks, ignore)
+                else:
+                    shutil.copy2(srcname, dstname)
+                # XXX What about devices, sockets etc.?
+            except (IOError, os.error) as why:
+                logging.error(why, exc_info=True)
+                errors.append((srcname, dstname, str(why)))
+            # catch the Error from the recursive copytree so that we can
+            # continue with other files
+            except BaseException as err:
+                logging.error(err, exc_info=True)
+                errors.extend(err.args[0])
+        try:
+            shutil.copystat(src, dst)
+        except WindowsError:
+            # can't copy file access times on Windows
+            pass
+        except OSError as why:
+            logging.error(why, exc_info=True)
+            errors.extend((src, dst, str(why)))
+        if errors:
+            raise BaseException(errors) 
                 
     def show_download_status(self, a,b,c):
         '''''Callback function 
