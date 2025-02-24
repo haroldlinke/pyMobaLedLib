33981e0e8cf10914d2c1f656a34eeedf3f12dd70
haroldlinke <43720258+haroldlinke@users.noreply.github.com>
2025-02-24 18:25:56 +0100

    Add files via upload

---
+++ python/update2.py
@@ -0,0 +1,9 @@
+import os
+
+filedir = os.path.dirname(os.path.realpath(__file__))
+print("test python update: filedir")
+if os.path.exists(os.path.combine(filedir,"../python_update")):
+    print("umbennenen python in python_bak")
+    os.rename("../python", "../python_bak")
+    print("umbennenen python_update in python")
+    os.rename("../python_update", "../python")
